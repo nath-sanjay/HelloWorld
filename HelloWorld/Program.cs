0e75c2d1800a695e22ff23c8427cc1c3b5e471df
nath-sanjay <nath.sanjay@gmail.com>
2020-07-16 15:42:33 +0530

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,14 +6,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWhat is your full name according to your passport? ");
+            Console.WriteLine("\nWhat is your name? ");
             var name = Console.ReadLine();
             var date = DateTime.Now;
-            Console.WriteLine($"\nHello, {name} from AET Eurofins, on {date:d} at {date:t}!");
-            Console.Write("\nPress key to exit Now...Checking simple change to show changes july16_1");
+            Console.WriteLine($"\nHello, {name}, today is {date:d} and the time is {date:t}!");
+            Console.Write("\nPress key to exit Now...");           
+           // Console.Write("\nGoodBye Cruel world, especially 2020! Break on through");
+           // Console.Write("Lord of the rings");
             System.Threading.Thread.Sleep(8000);
-            Console.Write("\nGoodBye Cruel world, especially 2020! Break on through");
-            Console.Write("Lord of the rings");
             Console.ReadKey(true);
         }
     }
